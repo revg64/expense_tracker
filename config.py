28c6ea7c4448e3cdad76ddca713d48e6a65af9ab
revg64 <revanthganesh64@gmail.com>
2026-01-29 19:50:00 +0530

    Use environment variables for database

--- config.py
+++ config.py
@@ -1,10 +1,16 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = os.getenv(
-    "DATABASE_URL",
-    "mysql+pymysql://root:@localhost:3306/expense_manager"
-)
-
+SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = os.getenv("SECRET_KEY")
 
-SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
+# import os
+
+# SQLALCHEMY_DATABASE_URI = os.getenv(
+#     "DATABASE_URL",
+#     "mysql+pymysql://root:@localhost:3306/expense_manager"
+# )
+
+# SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
