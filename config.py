abde5d1744982b008be74200540a926552014b69
revg64 <165544682+revg64@users.noreply.github.com>
2026-05-02 10:16:46 +0530

    Clean up config.py by removing comments
    Removed commented-out code for database configuration.

--- config.py
+++ config.py
@@ -4,13 +4,3 @@ SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-# import os
-
-# SQLALCHEMY_DATABASE_URI = os.getenv(
-#     "DATABASE_URL",
-#     "mysql+pymysql://root:@localhost:3306/expense_manager"
-# )
-
-# SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-# SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
